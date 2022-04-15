8b7f5b3b6478b3fafab208ed2c1e5bfaa7cfff41
woojin126 <woojin126@naver.com>
2022-04-15 19:10:56 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlinx/kotlinx-coroutines-core
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2-native-mt")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/ThreadTest.kt
@@ -0,0 +1,11 @@
+
+
+fun main( args : Array<String>) {
+    printSum(1,2)
+}
+
+fun sum(a : Int, b : Int) =  a + b
+
+fun printSum(a : Int, b : Int) {
+    println("sum of $a and $b is ${a + b }")
+}
