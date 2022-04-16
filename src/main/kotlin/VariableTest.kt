5dad72c158fb97366b7ec40f02caaa0f8bf4f3b2
woojin126 <woojin126@naver.com>
2022-04-16 16:37:28 +0900

    코틀린 기본 변수 ,클래스 , 함수 사용법

--- src/main/kotlin/VariableTest.kt
+++ src/main/kotlin/VariableTest.kt
@@ -1,6 +1,9 @@
 
 
 fun main( args : Array<String>) {
+    incrementX()
+    incrementX()
+    println(x)
     printSum(1,2)
 }
 
@@ -9,3 +12,11 @@ fun sum(a : Int, b : Int) =  a + b
 fun printSum(a : Int, b : Int) {
     println("sum of $a and $b is ${a + b }")
 }
+
+val PI = 3.14
+var x = 0
+
+fun incrementX() {
+    x+=1
+}
+
