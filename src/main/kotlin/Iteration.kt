ace5cc896db8fc321e2a4483dfc64294a2cb7898
woojin126 <woojin126@naver.com>
2022-05-14 13:13:47 +0900

    코틀린 in 컬렉션이나 범위의 원소 검사

--- src/main/kotlin/Iteration.kt
+++ src/main/kotlin/Iteration.kt
@@ -19,7 +19,19 @@ fun collections() {
     }
 }
 
+fun isLetter(c: Char) = c in 'a' .. 'z' || c in 'A' .. 'Z'
+fun isNotDigit(c: Int) = c !in 0 .. 9
+
+fun recognize(c: Int?) = when(c) {
+    in 0 .. 9  -> "false"
+    in 10 .. 20 -> "false"
+    else -> "Not in bound"
+}
+
 fun main() {
     println(iter())
     print(collections())
+    println(isLetter('q'))
+    println(isNotDigit(9))
+    println(recognize(null))
 }
